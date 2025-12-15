3b8bd6521d3d9e64052562e05421e758da03f8ff
alanalzi <117884402+alanalzi@users.noreply.github.com>
2025-12-15 21:28:06 +0700

    update perubahan fix

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -11,7 +11,7 @@ function formatDateForInput(dateString) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return `${day}-${month}-${year}`;
 }
 
 export default function ProductDevelopmentPage() {
